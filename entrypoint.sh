42685f0f1f6861e3f803e759cd4b88bd2eda6f94
Yolanda Lopez Solis <yolandal@MacBook-Pro-de-Yolanda.local>
2018-02-19 23:38:47 -0600

    fix fluentd template

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,31 @@
 #!/usr/bin/dumb-init /bin/sh
-    echo "<source>" > /fluentd/etc/fluent.conf
-	echo "  @type tail" >> /fluentd/etc/fluent.conf
-	echo "  path ${LOG_PATH}" >> /fluentd/etc/fluent.conf
-	echo "  pos_file ${POS_FILE}" >> /fluentd/etc/fluent.conf
-	echo "  format json" >> /fluentd/etc/fluent.conf
-	echo "  tag ${TAG}" >> /fluentd/etc/fluent.conf
-	echo "  refresh_interval ${REFRESH_INTERVAL}" >> /fluentd/etc/fluent.conf
-	echo "</source>" >> /fluentd/etc/fluent.conf
-	echo "<match ${TAG}>" >> /fluentd/etc/fluent.conf
-	echo "  @type kinesis_firehose" >> /fluentd/etc/fluent.conf
-	echo "  region ${REGION}" >> /fluentd/etc/fluent.conf
-	echo "  delivery_stream_name ${DELIVERY_STREAM_NAME}" >> /fluentd/etc/fluent.conf
-	echo "  flush_interval ${FLUSH_INTERVAL}" >> /fluentd/etc/fluent.conf
-	echo "  chunk_limit_size ${CHUNK_LIMIT_SIZE}" >> /fluentd/etc/fluent.conf
-	echo "  flush_thread_interval ${FLUSH_THREAD_INTERVAL}" >> /fluentd/etc/fluent.conf
-	echo "  flush_thread_burst_interval ${FLUSH_THREAD_INTERVAL_BURST_INTERVAL}" >> /fluentd/etc/fluent.conf
-	echo "  flush_thread_count ${FLUSH_THREAD_COUNT}" >> /fluentd/etc/fluent.conf
-	echo "  <instance_profile_credentials>" >> /fluentd/etc/fluent.conf
-	echo "    ip_address 169.254.169.254." >> /fluentd/etc/fluent.conf
-	echo "    port 80" >> /fluentd/etc/fluent.conf
-	echo "  </instance_profile_credentials>" >> /fluentd/etc/fluent.conf
-	echo "</match>" >> /fluentd/etc/fluent.conf
-	echo "<system>" >> /fluentd/etc/fluent.conf
-	echo "  log_level trace" >> /fluentd/etc/fluent.conf
-	echo "</system>" >> /fluentd/etc/fluent.conf
+
+#Fluentd.conf template
+echo "<source>" > /fluentd/etc/fluent.conf
+echo "  @type tail" >> /fluentd/etc/fluent.conf
+echo "  path ${LOG_PATH}" >> /fluentd/etc/fluent.conf
+echo "  pos_file ${POS_FILE}" >> /fluentd/etc/fluent.conf
+echo "  format json" >> /fluentd/etc/fluent.conf
+echo "  tag ${TAG}" >> /fluentd/etc/fluent.conf
+echo "  refresh_interval ${REFRESH_INTERVAL}" >> /fluentd/etc/fluent.conf
+echo "</source>" >> /fluentd/etc/fluent.conf
+echo "<match ${TAG}>" >> /fluentd/etc/fluent.conf
+echo "  @type kinesis_firehose" >> /fluentd/etc/fluent.conf
+echo "  region ${REGION}" >> /fluentd/etc/fluent.conf
+echo "  delivery_stream_name ${DELIVERY_STREAM_NAME}" >> /fluentd/etc/fluent.conf
+echo "  flush_interval ${FLUSH_INTERVAL}" >> /fluentd/etc/fluent.conf
+echo "  chunk_limit_size ${CHUNK_LIMIT_SIZE}" >> /fluentd/etc/fluent.conf
+echo "  flush_thread_interval ${FLUSH_THREAD_INTERVAL}" >> /fluentd/etc/fluent.conf
+echo "  flush_thread_burst_interval ${FLUSH_THREAD_INTERVAL_BURST_INTERVAL}" >> /fluentd/etc/fluent.conf
+echo "  flush_thread_count ${FLUSH_THREAD_COUNT}" >> /fluentd/etc/fluent.conf
+echo "  <instance_profile_credentials>" >> /fluentd/etc/fluent.conf
+echo "    ip_address 169.254.169.254." >> /fluentd/etc/fluent.conf
+echo "    port 80" >> /fluentd/etc/fluent.conf
+echo "  </instance_profile_credentials>" >> /fluentd/etc/fluent.conf
+echo "</match>" >> /fluentd/etc/fluent.conf
+echo "<system>" >> /fluentd/etc/fluent.conf
+echo "  log_level trace" >> /fluentd/etc/fluent.conf
+echo "</system>" >> /fluentd/etc/fluent.conf
 
 uid=${FLUENT_UID:-1000}
 
